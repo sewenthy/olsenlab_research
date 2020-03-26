4c44fab71d3849d87104870237a1be6de2b3ad2a
pi@rp9 <sewenthy@gmail.com>
2020-03-26 10:28:26 +0800

    prev

--- geophone.py
+++ geophone.py
@@ -5,14 +5,14 @@ import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+import matplotlib.animation as animation 
 from matplotlib import style
 
 value_fig = plt.figure()
-value_ax = fig.add_subplot(1,1,1)
+value_ax = value_fig.add_subplot(1,1,1)
 
 voltage_fig = plt.figure()
-voltage_ax = fig.add_subplot(1,1,1)
+voltage_ax = voltage_fig.add_subplot(1,1,1)
 
 i2c = busio.I2C(board.SCL, board.SDA)
 
@@ -24,23 +24,23 @@ channel = AnalogIn(ads, ADS.P0) #0
 
 
 def animate_value(i):
-	xs = []
-	values = []
-	while len(values)<100:
-		xs.append(time.monotonic())
-	    values.append(channel.value)
+    xs = []
+    values = []
+    while len(values)<100:
+        xs.append(time.monotonic())
+        values.append(channel.value)
 
-    value_ax = clear()
+    value_ax.clear()
     value_ax.plot(xs, values)
 
 def animate_voltage(i):
-	xs = []
-	voltages = []
-	while len(voltages)<100:
-		xs.append(time.monotonic())
-	    voltages.append(channel.voltage)
+    xs = []
+    voltages = []
+    while len(voltages)<100:
+        xs.append(time.monotonic())
+        voltages.append(channel.voltage)
 
-    voltage_ax = clear()
+    voltage_ax.clear()
     voltage_ax.plot(xs, voltages)
 
 ani_value = animation.FuncAnimation(value_fig,animate_value)
