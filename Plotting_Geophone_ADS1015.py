49b9cb88c9fcb44e106116b004f4d7f1ef04fc46
pi@rp9 <sewenthy@gmail.com>
2020-04-02 10:54:28 +0800

    real time writing and reading with continous signal done on rpi--next step server

--- Plotting_Geophone_ADS1015.py
+++ Plotting_Geophone_ADS1015.py
@@ -9,10 +9,15 @@ import numpy as np
 
 
 style.use("ggplot")
+__where = 0
 
-def readFileAuto():
+def findLastFile():
     files = glob.glob("{}/logs/Geophone_*.csv".format(os.getcwd()))
-    readFile(files[0])
+    return files[0]
+
+def readFileAuto(**kwargs):
+    files = glob.glob("{}/logs/Geophone_*.csv".format(os.getcwd()))
+    return readFile(files[0],**kwargs)
     
 def readFile(filename, waittime=0.14, timestamp = False, xs=None, vals=[], vols=[]):
     if timestamp:
@@ -35,4 +40,69 @@ def readFile(filename, waittime=0.14, timestamp = False, xs=None, vals=[], vols=
                 val, vol = line.split(",")
             vals.append(int(val))
             vols.append(float(vol))
-                
+            
+def readLine(fileStream, waittime=0.14, timestamp = False):
+    global __where
+    fileStream.seek(__where)
+    print("First where: {}".format(__where))
+    while 1:
+        line = fileStream.readline()
+        __where = fileStream.tell()
+        print("Second where: {}".format(__where))
+
+        if not line:
+            time.sleep(waittime)
+            fileStream.seek(__where)
+            continue
+        vol = None
+        val = None
+        x = None
+        if timestamp:
+            x,val,vol = line.split(",")
+            #xs.append(x)
+        else:
+            val, vol = line.split(",")
+        #vals.append(int(val))
+        #vols.append(float(vol))
+        return (x,[int(val),float(vol[:-2])],__where)
+    
+def animate(i,ax,xs,ys,val_vol,fileStream,timestamp,segment):
+    for j in range(segment):
+        x,res,where = readLine(fileStream=fileStream,timestamp=timestamp)
+        if timestamp:
+            xs.append(x)
+        ys.append(res[val_vol])
+        global __where
+        print((__where,where,res))
+    
+    ax.clear()
+    #NOISE LEVEL 1320,1460
+    ax.set_ylim(-1000,5000)
+    ys = ys[-3000:]
+    if timestamp:
+        xs = xs[-3000:]
+        ax.plot(xs, ys)
+    else:
+        ax.plot(ys)
+    strings = ["Raw Value","Voltage"]
+    stringTime = ""
+    xlab =""
+    if timestamp:
+        stringTime = " against Time"
+        xlab = "Time"
+    plt.title(["{}{}".format(strings[val_vol],stringTime)])
+    plt.xlabel(xlab)
+    plt.ylabel(strings[val_vol])
+    
+def plotting(val_vol,timestamp,file = None,interval=1,segment=100):
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    if file == None:
+        file = findLastFile()
+    
+    with open(file,"r") as f:
+        f.readline()
+        global __where
+        __where = f.tell()
+        ani = animation.FuncAnimation(fig,animate,fargs=(ax,[],[],val_vol,f,timestamp,segment),interval=interval)
+        plt.show()
